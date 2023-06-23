b450c5912e6f071f7df9f0af1174f5af60b13d65
Yong Wu <13618379679@163.com>
2023-06-23 18:13:54 +0800

    通过工作单元上下文提供者获取到的数据库上下文对外提供事务和连接给ADO.Net原生操作使用，共同完成事务没有问题，可以对这种方式ADO.Net对应的CRUD进行进一步的封装，完成相应与仓储一起的共享事务操作

--- demo/OpenDeepSpaceEntityFrameworkCore.Test/Controllers/UnitOfWorkController.cs
+++ demo/OpenDeepSpaceEntityFrameworkCore.Test/Controllers/UnitOfWorkController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MySqlConnector;
 using OpenDeepSpace.EntityFrameworkCore;
 using OpenDeepSpace.NetCore.Hangfire;
+using System.Data.SqlClient;
 
 namespace OpenDeepSpaceEntityFrameworkCore.Test.Controllers
 {
@@ -18,13 +23,16 @@ namespace OpenDeepSpaceEntityFrameworkCore.Test.Controllers
 
         IServiceScopeFactory serviceScopeFactory;
 
-        public UnitOfWorkController(IUnitOfWork unitOfWork, IRepository<CustomDbContext, Role> roleRepo, IRepository<OtherDbContext, Role> otherRoleRepo, IBackgroundJobManager backgroundJobManager, IServiceScopeFactory serviceScopeFactory)
+        IUnitOfWorkDbContextProvider<CustomDbContext> unitOfWorkDbContextProvider;
+
+        public UnitOfWorkController(IUnitOfWork unitOfWork, IRepository<CustomDbContext, Role> roleRepo, IRepository<OtherDbContext, Role> otherRoleRepo, IBackgroundJobManager backgroundJobManager, IServiceScopeFactory serviceScopeFactory, IUnitOfWorkDbContextProvider<CustomDbContext> unitOfWorkDbContextProvider)
         {
             this.unitOfWork = unitOfWork;
             this.roleRepo = roleRepo;
             this.otherRoleRepo = otherRoleRepo;
             this.backgroundJobManager = backgroundJobManager;
             this.serviceScopeFactory = serviceScopeFactory;
+            this.unitOfWorkDbContextProvider = unitOfWorkDbContextProvider;
         }
 
         [HttpGet]
@@ -88,6 +96,37 @@ namespace OpenDeepSpaceEntityFrameworkCore.Test.Controllers
                 unitOfWork.Commit();
             }
 
+
+        }
+
+        /// <summary>
+        /// 测试与Ado.Net原生操作共享连接的事务
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task TestAdoNetShareConnectionUow()
+        {
+
+            //从工作单元上下文提供者中 获取一个数据库上下文事务以及连接
+           var dbContext = await unitOfWorkDbContextProvider.GetDbContextAsync();
+
+           //获取事务
+           var trans = dbContext.Database.CurrentTransaction?.GetDbTransaction();
+
+            //获取连接
+            var conn = dbContext.Database.GetDbConnection();
+           
+            //ADO.Net原生相应的语句执行
+            var command = conn.CreateCommand();
+            command.Transaction = trans;
+            command.CommandText = "delete from role";
+            await command.ExecuteNonQueryAsync();
+               
+            //EFCore仓储语句执行
+            roleRepo.Insert(new Role() { Id = Guid.NewGuid(), RoleName = $"角色{Guid.NewGuid()}" });
+            //await otherRoleRepo.InsertAsync(new Role() { Id = Guid.NewGuid(), RoleName = $"一个异常的角色{Guid.NewGuid()}{Guid.NewGuid()}" });
+
+            await unitOfWork.CommitAsync();
 
         }
     }
