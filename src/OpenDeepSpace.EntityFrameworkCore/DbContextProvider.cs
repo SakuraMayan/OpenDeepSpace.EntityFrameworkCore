6f996eb99513fd584685f237fb1dd4835a18173a
Yong Wu <13618379679@163.com>
2023-12-16 13:26:52 +0800

    增加上下文提供者 方便拓展使用泛型类型的数据库上下文

---
+++ src/OpenDeepSpace.EntityFrameworkCore/DbContextProvider.cs
@@ -0,0 +1,64 @@
+﻿/*=================================================================================================
+
+=====================================Copyright Create Declare Start================================
+
+Copyright © 2023 by OpenDeepSpace. All rights reserved.
+Author: OpenDeepSpace	
+CreateTime: 2023/12/16 12:13:33	
+CLR: 4.0.30319.42000	
+Description:
+
+
+=====================================Copyright Create Declare End==================================
+
+=====================================Modify Records Explain Start==================================
+
+Modifier:	
+ModificationTime:
+Description:
+
+
+===================================================================================================
+
+Modifier:
+ModificationTime:
+Description:
+
+
+=====================================Modify Records Explain End====================================
+
+===================================================================================================*/
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDeepSpace.EntityFrameworkCore
+{
+    /// <summary>
+    /// 数据库上下文提供者
+    /// </summary>
+    public class DbContextProvider<TDbContext> : IDbContextProvider<TDbContext> where TDbContext : DbContext
+    {
+
+        IDbContextTransactionManager transactionManager;
+
+        TDbContext _dbContext;
+
+        public DbContextProvider(TDbContext dbContext, IDbContextTransactionManager transactionManager)
+        {
+            _dbContext = dbContext;
+            this.transactionManager = transactionManager;
+        }
+
+        public TDbContext GetDbContext()
+        {
+            transactionManager.AddDbContextWithBeginTransaction(_dbContext);
+
+            return _dbContext;
+        }
+    }
+}
---
+++ src/OpenDeepSpace.EntityFrameworkCore/IDbContextProvider.cs
@@ -0,0 +1,45 @@
+﻿/*=================================================================================================
+
+=====================================Copyright Create Declare Start================================
+
+Copyright © 2023 by OpenDeepSpace. All rights reserved.
+Author: OpenDeepSpace	
+CreateTime: 2023/12/16 11:47:10	
+CLR: 4.0.30319.42000	
+Description:
+
+
+=====================================Copyright Create Declare End==================================
+
+=====================================Modify Records Explain Start==================================
+
+Modifier:	
+ModificationTime:
+Description:
+
+
+===================================================================================================
+
+Modifier:
+ModificationTime:
+Description:
+
+
+=====================================Modify Records Explain End====================================
+
+===================================================================================================*/
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDeepSpace.EntityFrameworkCore
+{
+    public interface IDbContextProvider<TDbContext> where TDbContext:DbContext
+    {
+        TDbContext GetDbContext();
+    }
+}
